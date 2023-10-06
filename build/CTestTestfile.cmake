a5e1c50c275d9fc57a9be5bc2cb73f8cce0e4e6f
Dean <eastbright99@gmail.com>
2023-10-06 09:48:42 -0700

    assn2

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dean/ws_assn2/src
+# Build directory: /home/dean/ws_assn2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("assn2")
---
+++ build/assn2/CMakeFiles/driver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/dean/ws_assn2/devel/lib/assn2/driver"
+  "/home/dean/ws_assn2/devel/lib/assn2/driver.pdb"
+  "CMakeFiles/driver.dir/src/driver.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/driver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
