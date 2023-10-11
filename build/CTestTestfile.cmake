41a99bd9f279c032fea35132d8a0696796b179de
Dean <eastbright99@gmail.com>
2023-10-10 17:41:12 -0700

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dean/CPE-476_Assignment_Submissions/assn1/src
-# Build directory: /home/dean/CPE-476_Assignment_Submissions/assn1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("assn1")
