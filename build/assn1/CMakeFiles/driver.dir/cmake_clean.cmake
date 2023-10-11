f77a6e62368cdf4c92aa581d328e501554758429
Dean <eastbright99@gmail.com>
2023-10-10 17:40:17 -0700

    assn1

---
+++ build/assn1/CMakeFiles/driver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/dean/CPE-476_Assignment_Submissions/assn1/devel/lib/assn1/driver"
+  "/home/dean/CPE-476_Assignment_Submissions/assn1/devel/lib/assn1/driver.pdb"
+  "CMakeFiles/driver.dir/src/driver.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/driver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
