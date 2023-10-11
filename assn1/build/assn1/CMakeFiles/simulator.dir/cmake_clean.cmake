64cc0ab617770e62667715d7c205f80fb82b4129
Dean <eastbright99@gmail.com>
2023-10-10 17:44:24 -0700

    assn1

---
+++ assn1/build/assn1/CMakeFiles/simulator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/dean/CPE-476_Assignment_Submissions/assn1/devel/lib/assn1/simulator"
+  "/home/dean/CPE-476_Assignment_Submissions/assn1/devel/lib/assn1/simulator.pdb"
+  "CMakeFiles/simulator.dir/src/simulator.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ assn1/build/assn1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dean/CPE-476_Assignment_Submissions/assn1/src/assn1
+# Build directory: /home/dean/CPE-476_Assignment_Submissions/assn1/build/assn1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
