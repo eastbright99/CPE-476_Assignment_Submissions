274b9d1673f2b953c5d2138b0e968e8cb6b3e326
Dean <eastbright99@gmail.com>
2023-10-10 17:38:11 -0700

    Delete assn1 directory

--- assn1/build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/dean/ws_assn1/assn1/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
